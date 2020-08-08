fde6833e3be38cb3182718019943e4230e3fabd6
lsamano <leizl.samano@gmail.com>
2020-08-08 14:18:51 -0400

    added useUnifiedTopology

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ dotenv.config()
 const API_PORT = process.env.PORT || 3000
 const DB_URI = process.env.DB_URI
 
-mongoose.connect( DB_URI, { useNewUrlParser: true } )
+mongoose.connect(DB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
